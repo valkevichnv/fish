22980a9a6979b35d40515fcf571eb0a32ce6a56e
Nikolai Valkevich <valkevichnv@gmail.com>
2019-08-28 13:38:55 +0500

    added pull, conn and push commandsd to pak

--- pak.fish
+++ pak.fish
@@ -1,4 +1,17 @@
 function pak
-  set TARGET "claims-0$argv"
-  ssh valkevich@$TARGET.sd.kontur.ru
+  
+  set CMD $argv[1]
+  set HOST $argv[2]
+  set FROMPATH $argv[3]
+  set TOPATH $argv[4]
+  set UFQDN "valkevich@claims-0$HOST.sd.kontur.ru"
+
+  switch (echo $CMD)
+  case pull
+    scp $UFQDN:$FROMPATH $TOPATH
+  case push
+    scp $FROMPATH $UFQDN:$TOPATH
+  case conn
+    ssh $UFQDN
+  end
 end
